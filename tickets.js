238c614b4752365fc7a43e914bdc32ff4ca82f1b
Eif Taboada <eiftaboada@gmail.com>
2026-09-23 19:23:59 +0800

    Add 50% VIP surcharge for premium seating - taboada.eif

--- tickets.js
+++ tickets.js
@@ -2,8 +2,14 @@ function isValidQuantity(quantity) {
   return quantity > 0 && quantity <= 20;
 }
 
-function calculateTicketPrice(quantity, basePrice) {
-  return Math.floor(quantity * basePrice);
+function calculateTicketPrice(quantity, basePrice, seating = 'standard') {
+  let price = Math.floor(quantity * basePrice);
+
+  if (seating === 'premium') {
+    price *= 1.50;
+  }
+
+  return price;
 }
 
 module.exports = { isValidQuantity, calculateTicketPrice };
